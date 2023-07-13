8b5c3f4a5fd677a9cbf9cdb6fa52e708c7d8b911
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-13 16:17:18 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(todo = 'roller-skate') {
+  return `This Saturday, I want to ${todo}!`;
+}
+function mondayWork(work = 'go to the office') {
+  return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
